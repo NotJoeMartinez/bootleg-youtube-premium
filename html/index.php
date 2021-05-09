824808d84828a8feceee06d8316c21ca461386bc
notjoemartinez <notjoemartinez@protonmail.com>
2021-05-08 21:54:08 -0500

    Eddeted layout of php file

--- html/index.php
+++ html/index.php
@@ -1,24 +1,29 @@
 <!DOCTYPE html>
-
 <html>
-<head>
 <title>Bootlet youtube</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
-<style>
-    body {
-        width: 35em;
-        margin: 0 auto;
-        font-family: Tahoma, Verdana, Arial, sans-serif;
-    }
-</style>
-</head>
-<body>
+<div class="container">
+    <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg>
+        <span class="fs-4">Bootleg Youtube</span>
+      </a>
+
+      <ul class="nav nav-pills">
+        <li class="nav-item"><a href="/movies" class="nav-link">Library</a></li>
+      </ul>
+    </header>
+
+</div>
+
+  <body>
+
 	<h1>Bootleg youtube time<h1>
 
 <video width="500" height="500" controls>
   <source src="movie.mp4" type="video/mp4">
-Your browser does not support the video tag.
 </video>
 
 </body>
+
 </html>
