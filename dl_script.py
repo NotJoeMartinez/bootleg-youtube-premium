cd25af77fab72e60e0300bbebb183b9fa9c4416f
notjoemartinez <notjoemartinez@protonmail.com>
2021-05-09 21:14:04 -0500

    added a bunch of python stuff

---
+++ dl_script.py
@@ -0,0 +1,88 @@
+import sqlite3 
+import os
+import sys 
+import subprocess
+import datetime as dt
+import jinja2
+from youtube_dl import YoutubeDL
+
+def main():
+    now = dt.datetime.now().strftime("%m_%d_%-I:%M:%S%p")
+    url = sys.argv[1]
+
+    download_video(url,now)
+    log_downloads(url, now)
+    make_html()
+
+
+
+def download_video(url, now):
+    subprocess.run(f"youtube-dl -f mp4 {url} -o webapp/videos/{now}.mp4", shell=True)
+
+
+def make_html():
+    templateLoader = jinja2.FileSystemLoader(searchpath="./")
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    TEMPLATE_FILE = "templates/index.html"
+    template = templateEnv.get_template(TEMPLATE_FILE)
+
+
+    most_recent = find_most_recent("webapp/videos")
+    outputText = template.render(most_recent=most_recent)
+
+    html_file = open('webapp/index.html', 'w')
+    html_file.write(outputText)
+    html_file.close()
+
+
+
+def find_most_recent(directory):
+
+    now = dt.datetime.now()
+    dir_list = os.listdir(directory)
+    datetimes = []
+
+    for x in dir_list:
+        x = x[:-4]
+        dir_dt = dt.datetime.strptime(x, "%m_%d_%I:%M:%S%p")
+        datetimes.append(dir_dt)
+
+    most_recent = max(dt for dt in datetimes if dt < now)
+    mr = most_recent.strftime("%m_%d_%-I:%M:%S%p")
+    return mr    
+
+
+
+def log_downloads(url, now):
+    youtube_dl_opts = {}
+    with YoutubeDL(youtube_dl_opts) as ydl:
+        info_dict = ydl.extract_info(url, download=False)
+        video_url = info_dict.get("url", None)
+        video_id = info_dict.get("id", None)
+        video_title = info_dict.get('title', None)
+
+    add_to_db(now, url, video_id, video_title)
+
+
+
+def add_to_db(now, url, id, title):
+    con = sqlite3.connect('history.db')
+    cur = con.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS history 
+               (date text, url text, id text, title text)''')
+
+    cur.execute(f'''INSERT INTO history VALUES ("{now}","{url}","{id}","{title}")''')
+    con.commit()
+    con.close()
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
